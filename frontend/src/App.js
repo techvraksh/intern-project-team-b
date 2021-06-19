c9c86c01d37bf424284a3508e22893cccd3b6aab
Srashti Jain <srashti0204@gmail.com>
2021-06-19 18:02:30 +0530

    Revert "Add files via upload"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
-import styled from "styled-components";
-import Navbar from "./Navbar";
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-const App = () => {
+
+
+function App() {
   return (
-    <Container>
-      <Navbar />
-    </Container>
+    <BrowserRouter>
+ <div className="grid-container">
+  <main>
+    <Route path ="/product/:id" component ={ProductScreen}></Route>
+    <Route path =  "/" component={HomeScreen} exact></Route>
+  </main>
+  <footer className="row center">Techvkrash reserved</footer>
+</div>
+</BrowserRouter> 
   );
-};
+}
 
-
-const Container = styled.div`
-background: #67bc98;
-height: 100vh;
-`;
-
-export default App ;
+export default App;
