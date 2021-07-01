5a0b7ad52b27f4180c2b4716a85b618a08757cd3
Vamsi Kommineni <81870728+vamsikommineni222@users.noreply.github.com>
2021-07-01 17:56:18 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,112 @@
 import React from "react";
-import styled from "styled-components";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter,Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import SigninScreen from './screens/SigninScreen';
 import Navbar from "./components/Navbar";
-import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import About from "./screens/About";
+import CartScreen from "./screens/CartScreen";
+import ShippingAddressScreen from "./screens/ShippingAdressScreen";
 
-function App() {
+class App extends React.Component {
+  constructor () {
+ super();
+    this.state = {
+      cartItems: [],
+    };
+  }
+  componentDidMount() {
+    if(localStorage.carts) {
+      this.setState({ cartItems: JSON.parse(localStorage.carts) || [] });
+    }
+    window.addEventListener('beforeunload',this.handleUpdateLocalStorage);
+  }
+  handleUpdateLocalStorage = () => {
+    localStorage.setItem('carts',JSON.stringify(this.state.cartItems));
+  }
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload',this.handleUpdateLocalStorage)
+  }
+  addToCartHandler = (p) =>{
+    let isPresent =
+     this.state.cartItems.findIndex(product => product._id === p._id) !== -1
+     if(isPresent){
+     this.incrementQuantity(p._id);
+     }
+     else{
+    this.setState((prevState) => ({
+      cartItems: prevState.cartItems.concat({...p, quantity: 1}),
+    }));}
+   };
+  
+   incrementQuantity = (_id) => {
+    this.setState((prevState) => {
+      let updatedCartItems = prevState.cartitems.map((p) => {
+        if(p._id === _id) {
+          return{
+            ...p,
+            quantity: p.quantity+1,
+          };
+        }
+        return p;
+      });
+      return {
+        cartItems: updatedCartItems,
+      };
+    });
+   };
+   decrementQuantity = (_id) => {
+    this.setState((prevState) => {
+      let updatedCartItems = prevState.cartitems.map((p) => {
+        if(p._id === _id) {
+          return{
+            ...p,
+            quantity: p.quantity-1,
+          };
+        }
+        return p;
+      });
+      return {
+        cartItems: updatedCartItems,
+      };
+    });
+   };
+   deleteItem = (_id) => {
+    this.setState((prevState) => {
+      let updatedCartItems = prevState.cartitems.filter((p) => {
+        return p._id !== _id
+      });
+      return{
+        cartItems: updatedCartItems,
+      };
+    });
+   };
+render() {
   return (
     <BrowserRouter>
       <div className="grid-container">
-        <Container>
+        <div>
           <Navbar></Navbar>
-        </Container>
+        </div>
         <main>
-          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}>addToCartHandler={this.addToCartHandler}</Route>
           <Route path="/" component={HomeScreen} exact></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/about" component={About}></Route>
           <Route path="/Shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/cart" component={CartScreen}>
+            cartItems={this.state.cartItems}
+            incrementQuantity={this.incrementQuantity}
+            decrementQuantity={this.decrementQuantity}
+            deleteItem={this.deleteItem}
+          </Route>
         </main>
-        <footer className="row center">Techvkrash reserved</footer>
+        <footer className="row center">TechVraksh reserved</footer>
       </div>
     </BrowserRouter>
-  );
-}
+    );
+  }
+};
 
-const Container = styled.div`
-height: auto;
-`;
 export default App;
+
