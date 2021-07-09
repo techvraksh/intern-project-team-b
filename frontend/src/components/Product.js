294c22aa1bd351b6b58968c164c20526b3bc38cd
Vamsi Kommineni <81870728+vamsikommineni222@users.noreply.github.com>
2021-07-09 19:28:16 +0530

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,12 +4,12 @@ import Rating from './Rating';
 export default function Product(props) {
   const { product } = props;
   return (
-    <div key={product._id} className="card">
-      <a href={`/product/${product._id}`}>
+    <div key={product.id} className="card">
+      <a href={`/product/${product.id}`}>
         <img className="medium" src={product.image} alt={product.name} />
       </a>
       <div className="card-body">
-        <a href={`/product/${product._id}`}>
+        <a href={`/product/${product.id}`}>
           <h2>{product.name}</h2>
         </a>
         
